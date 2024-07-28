baa1845b1b519170668f01237749a323d641cfed
irisakellia <149322062+irisakellia@users.noreply.github.com>
2024-07-28 15:30:41 +0200

    studpla

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+       <p className='text-orange-500'>hello world</p>
+      
+      </div>
+     
+    </>
+  )
+}
+
+export default App
