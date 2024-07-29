ce8fc706642b2d343610fbcb5fbe3a40682497b4
irisakellia <149322062+irisakellia@users.noreply.github.com>
2024-07-29 11:14:26 +0200

    [adding]

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,12 @@ function App() {
   return (
     <>
       <div>
-        <nav className='inline-flex'>
-          <img src={logo}/>
-          <p className='text-orange-500 mt-10'>My StudyLife</p>
-          <p>Scheduling</p>
-          <p>Tasks</p>
+        <nav className='inline-flex bg-gradient-to-r from-blue-900 via-blue-600 to-blue-300 w-full'>
+          <img src={logo} className='ml-4'/>
+          <p className='text-white mt-10 ml-2 font-bold text-xl '>My StudyLife</p>
+          <p className='text-white font-medium text-xl ml-96 mt-10'>Scheduling</p>
+          <p className='text-white font-medium text-xl ml-4 mt-10'>Tasks</p>
+          <button className='text-white bg-transparent text-lg '>Sign in</button>
         
         </nav>
        
