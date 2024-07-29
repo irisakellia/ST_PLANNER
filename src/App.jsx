7cda92fc1775bb493bb38f249c52e470c66b1edf
irisakellia <149322062+irisakellia@users.noreply.github.com>
2024-07-29 11:57:35 +0200

    [adding]

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 import logo from '../public/logo.png'
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
+import AlarmIcon from '@mui/icons-material/Alarm';
+import EventIcon from '@mui/icons-material/Event';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,11 +21,14 @@ function App() {
         </nav>
 <section className='bg-gradient-to-b from-blue-100 via-blue-200 to-blue-300 h-[700px]'> 
         <p className='pt-20 text-3xl font-extrabold ml-40 text-blue-950 '>Never forget a class or assignment again.</p>
-        <section className='inline-flex'>
-          <p className='p-6 py-px bg-white'>Scheduling</p>
-          <p className='p-6 py-px bg-white'>Tasks</p>
-          <p className='p-6 py-px bg-white'>Reminders</p>
+        <section className='inline-flex mt-20'>
+          <p className='p-8 py-4 bg-white ml-20 w-80 rounded-s-lg'> <CheckBoxIcon/> Scheduling</p>
+          <p className='p-8 py-4 bg-white ml-10 w-80 rounded-s-lg'> <AlarmIcon/> Tasks</p>
+          <p className='p-8 py-4 bg-white ml-10 w-80 rounded-s-lg'>  <EventIcon/> Reminders</p>
         </section>
+
+        <p className='mt-10 text-xl font-bold text-blue-950 ml-96'>The Ultimate Study app</p>
+        <p className=' ml  '>to keep track of their life</p>
         </section>
       
        
