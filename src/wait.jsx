6571c5dbfa34162e75719879f3908193ed18fcce
irisakellia <149322062+irisakellia@users.noreply.github.com>
2024-07-29 12:07:55 +0200

    [adding]

---
+++ src/wait.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import cartoon from '../public/cartoon.avif'
+
+const wait = () => {
+  return (
+    <div>
+      <img src=''/>
+    </div>
+  )
+}
+
+export default wait
