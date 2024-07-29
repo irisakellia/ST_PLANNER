b7326c1ed0ebb9bf3e1fe6cf5442a0384916d949
irisakellia <149322062+irisakellia@users.noreply.github.com>
2024-07-29 12:15:16 +0200

    [adding]

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import { useState } from 'react'
 import Page from './Page'
+import {  createBrowserRouter,RouterProvider } from 'react-router-dom'
 
 
 function App() {
   const [count, setCount] = useState(0)
+  const routes  = createBrowserRouter([
+    {
+      path:'/',
+      element:<Page/>,
+    },
+    {
+      path:'/wait',
+      element:<Wait/>,
+    }
+  ])
+
 
   return (
     <>
-<Page/>
+<RouterProvider router={routes}/>
      </>
   )
 }
