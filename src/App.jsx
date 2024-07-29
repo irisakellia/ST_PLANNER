99e8f723290ffba63d7ebff33c07d7d9a5dffd8e
irisakellia <149322062+irisakellia@users.noreply.github.com>
2024-07-29 12:03:52 +0200

    [adding]

--- src/App.jsx
+++ src/App.jsx
@@ -22,13 +22,13 @@ function App() {
 <section className='bg-gradient-to-b from-blue-100 via-blue-200 to-blue-300 h-[700px]'> 
         <p className='pt-20 text-3xl font-extrabold ml-40 text-blue-950 '>Never forget a class or assignment again.</p>
         <section className='inline-flex mt-20'>
-          <p className='p-8 py-4 bg-white ml-20 w-80 rounded-s-lg'> <CheckBoxIcon/> Scheduling</p>
-          <p className='p-8 py-4 bg-white ml-10 w-80 rounded-s-lg'> <AlarmIcon/> Tasks</p>
-          <p className='p-8 py-4 bg-white ml-10 w-80 rounded-s-lg'>  <EventIcon/> Reminders</p>
+          <p className='p-8 py-4 bg-white ml-20 w-80 rounded-s-lg cursor-pointer'> <CheckBoxIcon/> Scheduling</p>
+          <p className='p-8 py-4 bg-white ml-10 w-80 rounded-s-lg cursor-pointer'> <AlarmIcon/> Tasks</p>
+          <p className='p-8 py-4 bg-white ml-10 w-80 rounded-s-lg cursor-pointer'>  <EventIcon/> Reminders</p>
         </section>
 
         <p className='mt-10 text-xl font-bold text-blue-950 ml-96'>The Ultimate Study app</p>
-        <p className=' ml  '>to keep track of their life</p>
+        <p className=' ml-[400px] text-black mt-4 text-xs   '>to keep track of their life</p>
         </section>
       
        
