73c2b3790c28af6fd59ad4e7fa4cfdce13af55c0
irisakellia <149322062+irisakellia@users.noreply.github.com>
2024-07-30 14:01:39 +0200

    [adding]

--- planner_back/app.js
+++ planner_back/app.js
@@ -1,16 +1,17 @@
 const express = require ('express');
 const port = 6000 ;
 const {dbConnection} = require('./connection/dbconnection');
-const router = require('./routes/siroute');
+const signuprouter = require('./routes/signup');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
 dbConnection();
 
-app.use(bodyParser.json());
-app.use('/api',router )
 app.use(cors())
+app.use(bodyParser.json());
+app.use('/api',signuprouter )
+
 
 
 app.listen(port, ()=>{
