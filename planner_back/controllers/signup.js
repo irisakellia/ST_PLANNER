c6f3a1dac8797acb7904c5b289d5caa5269b8a23
irisakellia <149322062+irisakellia@users.noreply.github.com>
2024-07-30 13:37:22 +0200

    [adding]

--- planner_back/controllers/signup.js
+++ planner_back/controllers/signup.js
@@ -6,10 +6,13 @@ async function makeUser(req,res){
         
         const{name,email,password} = req.body;
         const hpassword = await bcrypt.hash(password, 10);
+        const newUser = await User.create({
+            name,email,password:hpassword,
+        });
 
         return res.status(200).json({
             success:true,
-            message:`user has been successfully created `
+            message:`${newUser} has been successfully created `
         })
 
 
