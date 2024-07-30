58751e422e95bd8c79e5978a1e31b8ee20671ce0
irisakellia <149322062+irisakellia@users.noreply.github.com>
2024-07-30 14:11:58 +0200

    [adding]

---
+++ planner_back/utils/jwtUtils.js
@@ -0,0 +1,13 @@
+
+const jwt = require('jsonwebtoken');
+const secretKey = require('../dbconnection/jwtconfig');
+
+function generateToken(user) {
+  const payload = {
+    id: user._id,
+    email: user.email,
+  };
+  return jwt.sign(payload, secretKey, { expiresIn: '1h' });
+}
+
+module.exports = { generateToken };
