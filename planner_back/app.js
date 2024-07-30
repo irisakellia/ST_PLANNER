ecff541d055c82dc32bd9981f65f97b4513561a5
irisakellia <149322062+irisakellia@users.noreply.github.com>
2024-07-30 13:15:44 +0200

    [adding]

--- planner_back/app.js
+++ planner_back/app.js
@@ -1,7 +1,10 @@
 const express = require ('express');
 const port = 6000 ;
+const {dbConnection} = require('./connection/dbconnection')
 
 const app = express();
+dbConnection;
+
 
 app.listen(port, ()=>{
     console.log(`Our app is running on port ${port}`);
---
+++ planner_back/connection/dbconnection.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const uri = "mongodb+srv://irisakellia:123@cluster0.jhv2iwc.mongodb.net/stupla?retryWrites=true&w=majority&appName=Cluster0";
+
+const dbConnection = ()=>{
+
+    mongoose.connect(uri);
+
+    mongoose.connection.on("connected", ()=>{
+        console.log("successfully connected to the database ");
+    })
+
+    mongoose.connection.on("disconnection",()=>{
+        console.log("a disconnection from the database ");
+    })
+
+    mongoose.connection.on(error,()=>{
+        console.error(error.message);
+    })
+}
+
+module.exports = {dbConnection}
